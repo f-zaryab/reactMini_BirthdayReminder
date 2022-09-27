838b4675ae96cd3475910f85129e9759c78f9d42
f-zaryab <zaryabfarooqui@gmail.com>
2022-09-28 03:22:18 +0500

    Updates

---
+++ precache-manifest.a0267b19b985814cf91fa54df7731342.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbc44bc3d5b834671e54338d6e9bdcc5",
+    "url": "/reactMini_BirthdayReminder/index.html"
+  },
+  {
+    "revision": "20a77fe8c6a23b543adf",
+    "url": "/reactMini_BirthdayReminder/static/css/main.f24842c0.chunk.css"
+  },
+  {
+    "revision": "6e2d7a9fbc772ec3686f",
+    "url": "/reactMini_BirthdayReminder/static/js/2.1f771bf1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactMini_BirthdayReminder/static/js/2.1f771bf1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20a77fe8c6a23b543adf",
+    "url": "/reactMini_BirthdayReminder/static/js/main.f0c139c5.chunk.js"
+  },
+  {
+    "revision": "83bc98ddde3df862f925",
+    "url": "/reactMini_BirthdayReminder/static/js/runtime-main.0d7c8fdf.js"
+  }
+]);
